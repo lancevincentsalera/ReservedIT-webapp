00cfeafc38d7ed0874098085a93d01494077bf3d
Rodulph Cabaobao <cabaobao24@gmail.com>
2024-06-21 09:24:31 +0800

    Password

--- ASI.Basecode.Services/ServiceModels/UserViewModel.cs
+++ ASI.Basecode.Services/ServiceModels/UserViewModel.cs
@@ -16,6 +16,8 @@ namespace ASI.Basecode.Services.ServiceModels
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
+        [MinLength(8, ErrorMessage = "Password must be atleast 8 characters long.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{4,}$", ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")]
         public string Password { get; set; }
 
         [Required(ErrorMessage = "Confirmation Password is required.")]
