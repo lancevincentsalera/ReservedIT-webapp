d0fb994991d565608306edc915180ba8e6cdd127
Lance Vincent A. Salera <lancevincent.salera@cit.edu>
2024-07-03 15:27:00 +0800

    minor modal changes

--- ASI.Basecode.WebApp/wwwroot/js/modal.js
+++ ASI.Basecode.WebApp/wwwroot/js/modal.js
@@ -1,1 +1,26 @@
 ﻿
+const displayModal = () => {
+    $('#createUserModal').modal('show');
+}
+const displayConfirmationModal = (hideId, showId) => {
+    $(hideId).modal('hide');
+    $(showId).modal('show');
+}
+const hideConfirmationModal = (hideId, showId) => {
+    $(showId).modal('show');
+    $(hideId).modal('hide');
+}
+
+const submitForm = (formId, action, controller) => {
+    $.ajax({
+        url: `/${controller}/${action}`,
+        type: 'POST',
+        data: $(formId).serialize(),
+        success: (response) => {
+            console.log('Form submitted successfully');
+        },
+        error: function (xhr, status, error) {
+            console.error('Error submitting form:', error);
+        }
+    })
+}
