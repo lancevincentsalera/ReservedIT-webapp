5a368b9c2358befe640b62b63fa599d42f166d1f
Lance Vincent A. Salera <lancevincent.salera@cit.edu>
2024-07-03 12:41:02 +0800

    improved modals frontend  slightly

---
+++ ASI.Basecode.WebApp/wwwroot/js/dropdown.js
@@ -0,0 +1,34 @@
+﻿let dropdownTriggers = document.querySelectorAll('.dropdown-trigger');
+
+dropdownTriggers.forEach((trigger) => {
+    let dropdown = trigger.querySelector('.dropdown-action');
+    let editUserBtn = trigger.querySelector('.create-user-btn');
+
+    editUserBtn.addEventListener('click', (event) => {
+        event.stopPropagation();
+        $('#editUserModal').modal('show');
+    });
+
+    trigger.addEventListener('click', (event) => {
+        event.stopPropagation();
+
+        dropdownTriggers.forEach((otherTrigger) => {
+            if (otherTrigger !== trigger) {
+                let otherDropdown = otherTrigger.querySelector('.dropdown-action');
+                otherDropdown.classList.remove("show-dropdown");
+            }
+        });
+
+        dropdown.classList.toggle('show-dropdown');
+    });
+});
+
+document.addEventListener('click', (event) => {
+    dropdownTriggers.forEach((trigger) => {
+        let dropdown = trigger.querySelector('.dropdown-action');
+
+        if (!trigger.contains(event.target)) {
+            dropdown.classList.remove('show-dropdown');
+        }
+    });
+});
