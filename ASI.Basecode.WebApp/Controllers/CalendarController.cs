b90fbcae2fd8a441a83fe7c634fb3bab348e171d
Ethan <ethan.ortiz.21@usjr.edu.ph>
2024-07-17 04:49:20 +0800

    Unfinished calendar

---
+++ ASI.Basecode.WebApp/Controllers/CalendarController.cs
@@ -0,0 +1,42 @@
+﻿using ASI.Basecode.WebApp.Mvc;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace ASI.Basecode.WebApp.Controllers
+{
+    /// <summary>
+    /// Calendar Controller
+    /// </summary>
+    public class CalendarController : ControllerBase<CalendarController>
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="httpContextAccessor"></param>
+        /// <param name="loggerFactory"></param>
+        /// <param name="configuration"></param>
+        /// <param name="localizer"></param>
+        /// <param name="mapper"></param>
+        public CalendarController(IHttpContextAccessor httpContextAccessor,
+                              ILoggerFactory loggerFactory,
+                              IConfiguration configuration,
+                              IMapper mapper = null) : base(httpContextAccessor, loggerFactory, configuration, mapper)
+        {
+
+        }
+
+        /// <summary>
+        /// Returns Calendar View.
+        /// </summary>
+        /// <returns> Calendar View </returns>
+        public IActionResult Index(string year, string month, string day)
+        {
+            Console.WriteLine(Request.QueryString);
+            return View();
+        }
+    }
+}
